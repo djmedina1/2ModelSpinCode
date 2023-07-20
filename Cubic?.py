be107c077ed8fc014fae26ade64b427193f95b18
Diego <diegomedinamedia@gmail.com>
2023-07-20 10:29:40 +0200

    commiting files

---
+++ Cubic?.py
@@ -0,0 +1,48 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def calculate_energy_sweep(H, Phi_values, J_AF, M, Ku, K1, anisotropy_axis):
+    # Convert angles to radians
+    Phi_rad = np.radians(Phi_values)
+    
+    # Calculate the energy terms
+    phi_1 = Phi_rad[:-1]
+    phi_2 = Phi_rad[1:]
+    
+    energy_H = -0.5 * H * M * (np.cos(phi_1) + np.cos(phi_2))
+    energy_J_AF = -J_AF * np.cos(phi_1 - phi_2)
+    
+    ''' Uniaxial Term ''' 
+    #energy_anis = (1/8) * K1 * (np.sin(2 * (phi_1 - anisotropy_axis)) ** 2 + np.sin(2 * (phi_2 - anisotropy_axis)) ** 2)
+    energy_anis = 0.5 * Ku * (np.sin(Phi_rad[:-1] - anisotropy_axis) ** 2 + np.sin(Phi_rad[1:] - anisotropy_axis) ** 2)
+    energy_total = energy_H + energy_J_AF + energy_anis
+    
+    return energy_total
+
+# Example usage
+H_values = [0, 0.75, 1.0, 1.5]  # Array of applied magnetic field strengths
+Phi_values = np.linspace(0, 360, 100)  # Array of applied field angles in degrees
+J_AF = 1  # Antiferromagnetic coupling strength
+M = 0.2  # Magnitude of magnetization vectors in layers A in Tesla
+Ku = 1  # Anisotropy constant
+K1 = 1  # First-order anisotropy parameter
+anisotropy_axis = np.radians(45)  # Anisotropy axis angle in radians
+
+plt.figure()  # Create a new figure
+
+# Calculate and plot energy for each value of H
+for H in H_values:
+    energy_values = calculate_energy_sweep(0, Phi_values, J_AF, M, Ku, K1, anisotropy_axis)
+    energy_derivative = np.gradient(energy_values, np.radians(Phi_values[:-1]))
+    plt.plot(np.radians(Phi_values[:-1]), energy_derivative, label=f'H={H} (derivative)')
+
+# Set custom x-axis tick locations and labels
+x_ticks = np.arange(0, 361, 45) 
+plt.xticks(x_ticks)
+
+plt.xlabel('Applied Field Angle (degrees)')
+plt.ylabel('Energy')
+plt.title('Energy vs. Applied Field Angle for different H values')
+plt.legend()  # Show the legend
+plt.show()
---
+++ Example Format.py
@@ -0,0 +1,7 @@
+def Energy(diego):
+    diego = x,y,z
+    return dsghk;
+diego1=intial guess
+result=minmize(Energy,diego1)
+h = result.x
+print(h)
